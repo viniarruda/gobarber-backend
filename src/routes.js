3a49afd73fcc85af1a96be4808eeb38d7a21411d
Vinicius Arruda <viniarruda.souza@gmail.com>
2019-07-08 02:12:14 +0000

    feat(user): create user model and add bcrypt to encrypt password

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Normal route' });
-});
+routes.post('/users', UserController.store);
+
+routes.get('/users', UserController.show);
 
 export default routes;
