de453de62dcc0a52679a89ed1dceef904ff4875d
Vinicius Arruda <viniarruda.souza@gmail.com>
2019-08-05 03:32:00 +0000

    feat: create new fields

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -12,14 +12,14 @@ import CancellationMail from '../jobs/CancellationMail';
 
 class AppointmentController {
   async index(req, res) {
-    const { page = 1 } = req.query;
+    const { page = 1, limit = 15 } = req.query;
 
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
-      attributes: ['id', 'date'],
-      limit: 20,
-      offset: (page - 1) * 20,
+      attributes: ['id', 'date', 'past', 'cancelable'],
+      limit,
+      offset: (page - 1) * limit,
       include: [
         {
           model: User,
